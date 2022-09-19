9809ed12de3ef690ce01d43ecfd6386919175eb6
qu-ma <mattson.quentin@gmail.com>
2022-09-19 22:16:50 +0000

    changed function name

--- lab13.py
+++ lab13.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def main():
+def greeting():
     name = input("What is your name? ")
     day_of_week = input("What day of the week is it? ")
 
     print(f"Hello, {name}! Happy {day_of_week}!")
 
-main()
+greeting()
