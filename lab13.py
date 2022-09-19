8d7b8758363f58eab0cc76588edaa5c6fd938faa
qu-ma <mattson.quentin@gmail.com>
2022-09-19 22:47:20 +0000

    changed variable name

--- lab13.py
+++ lab13.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def main():
-    name = input("What is your name? ")
+    user_name = input("What is your name? ")
     day_of_week = input("What day of the week is it? ")
 
     print(f"Hello, {name}! Happy {day_of_week}!")
