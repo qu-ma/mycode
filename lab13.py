5fa97e0f101122ba780749baa95eab4f71cefd27
qu-ma <mattson.quentin@gmail.com>
2022-09-20 17:53:13 +0000

    mixed lists

--- lab13.py
+++ lab13.py
@@ -4,7 +4,7 @@ def main():
     user_name = input("What is your name? ")
     day_of_week = input("What day of the week is it? ")
 
-    print(f"Hello, {name}! Happy {day_of_week}!")
+    print(f"Hello, {user_name}! Happy {day_of_week}!")
 
 if __name__ == "__main__":
     main()
---
+++ mix-list/lab15-challenge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def main():
+    ip_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+    
+    print(ip_list[3:5])
+
+if __name__ == "__main__":
+    main()
