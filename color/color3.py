d41dfaca6fc12611ae6f88e0b78ea58bb4fe74ac
qu-ma <mattson.quentin@gmail.com>
2022-09-23 15:12:41 +0000

    more functions

---
+++ color/color3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import crayons
+
+print(f'{crayons.magenta("Hobak")} {crayons.cyan("Jun")}')
---
+++ quickzippy/createarchive.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import os
+import zipfile
+
+def zipdir(dirpath, zipfileobj):
+    for root, dirs, files in os.walk(dirpath):
+        for file in files:
+            print(os.path.join(root, file))
+            zipfileobj.write(os.path.join(root, file))
+    return None
+
+def main():
+    dirpath = input("Enter directory to archive data: ")
+
+    if os.path.isdir(dirpath):
+        zippedfn = input("What should we called the finished archive? ")
+        
+        with zipfile.ZipFile(zippedfn, "w", zipfile.ZIP_DEFLATED) as zipfileobj:
+            zipdir(dirpath, zipfileobj)
+
+    else:
+        print("Run the script again when you have a valid directory to zip")
+
+main()
